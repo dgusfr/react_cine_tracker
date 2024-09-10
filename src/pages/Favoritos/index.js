96aede196b31d7e75ab33c2f49480ff316e0a764
dgusfr <diegogustavo.fr@gmail.com>
2024-09-10 19:48:11 -0300

    refactor(favoritos-context): otimizar lógica de adição e remoção de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,24 +1,25 @@
-import Banner from 'components/Banner';
-import Card from 'components/Card';
-import Titulo from 'components/Titulo';
-import { useFavoritoContext } from 'contextos/Favoritos';
-import styles from './Favoritos.module.css';
+import Banner from "components/Banner";
+import Card from "components/Card";
+import Titulo from "components/Titulo";
+import { useFavoritoContext } from "contextos/Favoritos";
+import styles from "./Favoritos.module.css";
 
 function Favoritos() {
-    const { favorito } = useFavoritoContext();
-    return (
-        <>
-            <Banner imagem='favoritos' />
-            <Titulo>
-                <h1>Meus Favoritos</h1>
-            </Titulo>
-            <section className={styles.container}>
-                {favorito.map((fav) => {
-                    return <Card {...fav} key={fav.id} />
-                })}
-            </section>
-        </>
-    )
+  const { favorito } = useFavoritoContext();
+
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Filmes Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {favorito.map((filme) => {
+          return <Card {...filme} key={filme.id} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default Favoritos;
