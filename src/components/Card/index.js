eb7b9b8aca5128741b8e3cf31599beb8f6877f74
dgusfr <diegogustavo.fr@gmail.com>
2024-09-21 12:09:22 -0300

    feat(card): integrar contexto de filme ao componente Card e exibir nota dos filmes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,25 +1,37 @@
-import { useFavoritoContext } from "contextos/Favoritos";
+import { useEffect, useState } from "react";
 import styles from "./Card.module.css";
 import iconeFavoritar from "./favoritar.png";
 import iconeDesfavoritar from "./desfavoritar.png";
 import { Link } from "react-router-dom";
+import { useFilmeContext } from "contextos/FilmeContext";
 
 function Card({ id, titulo, capa }) {
-  const { favorito, adicionarFavorito } = useFavoritoContext();
-  const ehFavorito = favorito.some((fav) => fav.id === id);
-  const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+  const { carregarFilme, filme } = useFilmeContext();
+  const [nota, setNota] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await carregarFilme(id);
+      setNota(filme?.vote_average);
+    };
+
+    fetchData();
+  }, [id, carregarFilme, filme]);
 
   return (
     <div className={styles.container}>
       <Link className={styles.link} to={`/filmes/${id}`}>
         <img src={capa} alt={titulo} className={styles.capa} />
         <h2>{titulo}</h2>
+        {nota && <p>Nota: {nota}/10</p>}
       </Link>
       <img
-        src={icone}
+        src={!ehFavorito ? iconeFavoritar : iconeDesfavoritar}
         alt="Favoritar filme"
         className={styles.favoritar}
-        onClick={() => adicionarFavorito({ id, titulo, capa })}
+        onClick={() => {
+          adicionarFavorito({ id, titulo, capa });
+        }}
       />
     </div>
   );
