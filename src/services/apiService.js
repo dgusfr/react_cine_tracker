36552c933544d2ff180067114df910bb77c5a62a
dgusfr <diegogustavo.fr@gmail.com>
2024-09-20 21:28:01 -0300

    chore(api): adicionar serviço de integração com TMDB e armazenar chave de API no .env

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,18 +3,17 @@ import axios from "axios";
 const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 const baseURL = "https://api.themoviedb.org/3";
 
-// Função para buscar detalhes de um filme por ID
 export const buscarDetalhesDoFilme = async (id) => {
   try {
     const resposta = await axios.get(`${baseURL}/movie/${id}`, {
       params: {
         api_key: apiKey,
-        language: "pt-BR", // Escolha o idioma desejado
+        language: "pt-BR",
       },
     });
-    return resposta.data; // Retorna os dados do filme
+    return resposta.data;
   } catch (erro) {
     console.error("Erro ao buscar detalhes do filme:", erro);
-    throw erro; // Propaga o erro para ser tratado no componente
+    throw erro;
   }
 };
